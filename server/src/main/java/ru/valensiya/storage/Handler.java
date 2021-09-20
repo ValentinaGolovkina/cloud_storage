0e43d31d44334e35918432a356d6d84e7c77e227
VGolovkina <VGolovkina@cbgr.ru>
2021-09-20 13:34:03 +0300

    Отправка выбранного файла с клиента на сервер

--- server/src/main/java/ru/valensiya/storage/Handler.java
+++ server/src/main/java/ru/valensiya/storage/Handler.java
@@ -4,12 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.Socket;
 
 @Slf4j
 public class Handler implements Runnable {
 
     private final Socket socket;
+    private static final String ROOT_DIR = "server/root/";
+    private static final byte [] buffer = new byte[1024];
 
     public Handler(Socket socket) {
         this.socket = socket;
@@ -21,14 +25,23 @@ public class Handler implements Runnable {
 
     @Override
     public void run() {
-        try (DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-             DataInputStream is = new DataInputStream(socket.getInputStream())
+        try (DataInputStream is = new DataInputStream(socket.getInputStream())
         ) {
             while (true) {
-                String s = is.readUTF();
-                log.debug("Received: {}", s);
-                os.writeUTF(s);
-                os.flush();
+                String nameFile = is.readUTF();
+                log.debug("Received: {}", nameFile);
+                if (nameFile.startsWith("nameFile")) {
+                    int size = is.readInt();
+                    log.debug("Received size: {}", size);
+                    File file = new File(ROOT_DIR + nameFile.substring(10));
+                    file.createNewFile();
+                    try (FileOutputStream fos = new FileOutputStream(file)) {
+                        for(int i=0; i<size;i++){
+                            fos.write(is.read());
+                        }
+                        log.debug("файл закончился");
+                    }
+                }
             }
         } catch (Exception e) {
             log.error("stacktrace: ", e);
